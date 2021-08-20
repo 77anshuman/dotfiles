5769655f5d604138e7decd6bc0081ada5e851876
anshu <77anshuman@gmail.com>
2021-08-20 17:09:55 +0530

    adding neovim configuration files

---
+++ neovim/.config/nvim/lua/lualinesetup.lua
@@ -0,0 +1,25 @@
+require("lualine").setup {
+  options = { theme = "gruvbox" },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { { "branch" }, { "diff", colored = false } },
+    lualine_c = { { "filename", path = 1 } },
+    lualine_y = {
+      {
+        "encoding",
+        condition = function()
+          return vim.bo.fileencoding and #vim.bo.fileencoding > 0 and vim.bo.fileencoding ~= "utf-8"
+        end,
+      },
+      { "filetype", icons_enabled = false },
+    },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+}
